3755928ffee6aef6f83ac7d46e181d68db6ab1dc
rafaelmirandam <rcsilva12@gmail.com>
2018-01-20 10:58:13 -0200

    corrigindo proj

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,13 +3,13 @@ require 'capybara'
 require 'capybara/dsl'
 require 'capybara/rspec/matchers'
 require 'capybara/rspec/matcher_proxies'
-require 'capybara/poltergeist'
 require 'selenium-webdriver'
 require 'rspec'
 require 'site_prism'
 require 'faker'
+require 'allure-cucumber'
 
-DATA = YAML.load_file(File.dirname(__FILE__) + "/data/#{ENVIRONMENT_TYPE}.yaml")
+DATA = YAML.load_file(File.dirname(__FILE__) + "/data/qua.yaml")
 
 Capybara.configure do |c|
     c.default_driver = :selenium_chrome_headless
